6bd5184ae1e6364d8b7a1839335368c915cb629d
ppoya <temporary020627@gmail.com>
2026-05-15 05:47:21 +0900

    첫 커밋

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-pink-100 flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-pink-500">
+        냥쿠키
+      </h1>
+    </div>
+  )
+}
+
+export default App
